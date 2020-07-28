44abee0a671a63033de8959be117554f842a93c9
smirchan <smirchan@gmail.com>
2020-07-28 00:17:43 -0400

    Made homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className='Navbar-div'>
+                <ul id='nav'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
