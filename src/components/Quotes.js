f445356e32011fd1ec0427a954f3f00fdc564b09
rishimirchandani <70929395+rishimirchandani@users.noreply.github.com>
2025-07-22 13:04:12 +0100

    sub silverman quote for ian fountain

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -5,8 +5,8 @@ class Quotes extends React.Component {
         return (
             <div className="Section Quotes">
                 <i className="Anchor" id="about"></i>
-                <p className="quote">"A bold, individual musical voice."</p>
-                <p className="quoted">—Marc Silverman</p>
+                <p className="quote">"Such superlative conviction. True insight."</p>
+                <p className="quoted">—Ian Fountain</p>
                 <p className="quote">"A captivating gem. Electric and masterful."</p>
                 <p className="quoted">—Nancy Galbraith</p>
             </div>
