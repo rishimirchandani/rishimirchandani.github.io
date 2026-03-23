ee8b84d6fc208f69221028a94bf2a0e000913a19
Rishi Mirchandani <mirchandani.rs@gmail.com>
2026-03-23 23:38:12 +0000

    new bio, added media

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -13,9 +13,7 @@ function BioSection({ credit }) {
                     <p className="bio-text">
                         After amassing six gold medals in major national and international competitions, 
                         Indian-American pianist Rishi Mirchandani debuted as soloist with the Grammy 
-                        award-winning Pittsburgh Symphony Orchestra at the age of 18. He made his first 
-                        of four appearances at Carnegie Hall in New York at age 12, and he has performed 
-                        to critical acclaim in England, Germany, Italy, Portugal, Spain, and the United States.
+                        award-winning Pittsburgh Symphony Orchestra at the age of 18. He has performed to critical acclaim in Germany, Italy, Portugal, Spain, the UK, and the USA. He teaches piano at England's renowned Charterhouse School and concertises  as a soloist, chamber musician, and song pianist.
                     </p>
                     <Link to="/bio" className="bio-link">Read full bio →</Link>
                 </div>
